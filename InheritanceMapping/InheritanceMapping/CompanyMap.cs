032c251251fa6997909873bf8da73e5511e6213a
Tayebe Safari <tssafri@gmail.com>
2019-03-06 17:44:58 +0330

    Added Mapping Class

---
+++ InheritanceMapping/InheritanceMapping/CompanyMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentNHibernate.Mapping;
+
+namespace InheritanceMapping
+{
+    public class CompanyMap:ClassMap<Company>
+    {
+        public CompanyMap() 
+        {
+            Id(x => x.Id);
+            Map(x => x.CompanyID);
+            Map(x => x.CompanyName);
+            Map(x => x.EmployeeID);
+            Map(x => x.EmployeeName);
+            Map(x => x.EmailAddress);
+            Map(x => x.Address);
+            Map(x => x.JobTitle);
+            Map(x => x.PhoneNumber);
+
+            
+        }
+    }
+}
--- InheritanceMapping/InheritanceMapping/PersonMap.cs
+++ InheritanceMapping/InheritanceMapping/PersonMap.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
 
 namespace InheritanceMapping
 {
-    class PersonMap
+    public class PersonMap : ClassMap<Person>
     {
+        public PersonMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.NationalIdentityNumber);
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+            Map(x => x.PhoneNumber);
+            Map(x => x.Address);
+            Map(x => x.Birthdate);
+            Map(x => x.EmailAddress);
+            
+        }
     }
 }
