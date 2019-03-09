8d75de60922fe3526b99b2a68877401c27a830a7
pedrampzh <pzhpedram@gmail.com>
2019-03-09 11:58:23 +0330

    Change this file

---
+++ InheritanceMapping/InheritanceMapping/PartyMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMapping
+{
+    public class PartyMap : ClassMap<Party>
+    {
+        public PartyMap()
+            {
+                Table("Party");
+                Id(x => x.Id, "intId");
+                Map(x => x.EmailAddress);
+                Map(x => x.Address);
+                Map(x => x.PhoneNumber);
+                DiscriminateSubClassesOnColumn("Type").Not.Nullable();
+            }
+    }
+}
+
